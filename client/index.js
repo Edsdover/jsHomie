fb7c77007ce3899243133f33b2070090bf74f51b
Edwin Dover <edsdover@gmail.com>
2015-04-15 09:36:16 -0700

    adding stuff

--- client/index.js
+++ client/index.js
@@ -1,2 +1,32 @@
 'use strict';
-console.log('apollo -> online');
+
+$(document).ready(init);
+var numbers = [];
+
+function init(){
+  $('#return').click(printToPage);
+}
+
+function printToPage(){
+  var oldText = $('#output').text();
+  var value = $('input').val();
+  if (value === '='){
+    sumNumbers();
+  }else{
+    numbers.push(value);
+    console.log(sumNumbers(numbers));
+    $('#output').text(oldText + ' ' + value);
+    $('#input').val('');
+  }
+}
+
+function sumNumbers() {
+  var sum = numbers.reduce(function(a, b) {
+    a = parseInt(a);
+    b = parseInt(b);
+    return a + b;
+  });
+  var $div = $('<div>');
+  $div.text(sum);
+  $('#output').append($div);
+}
