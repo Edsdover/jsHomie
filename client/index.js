449b14a1cd467371dddb066e1f78989e3a9eca43
Edwin Dover <edsdover@gmail.com>
2015-04-15 11:45:34 -0700

    adding more math functions

--- client/index.js
+++ client/index.js
@@ -12,6 +12,12 @@ function printToPage(){
   var value = $('input').val();
   if (value === '='){
     sumNumbers();
+  }else if (value === '*'){
+    multiNums();
+  }else if (value === '/'){
+    divideNums();
+  }else if (value === '-'){
+    subNums();
   }else{
     numbers.push(value);
     $('#output').text(oldText + ' ' + value);
@@ -27,5 +33,38 @@ function sumNumbers() {
   });
   var $div = $('<div>');
   $div.text(sum);
+  $('#output').append($div);
+}
+
+function multiNums() {
+  var multi = numbers.reduce(function(a, b) {
+    a = parseInt(a);
+    b = parseInt(b);
+    return a * b;
+  });
+  var $div = $('<div>');
+  $div.text(multi);
+  $('#output').append($div);
+}
+
+function divideNums() {
+  var divide = numbers.reduce(function(a, b) {
+    a = parseInt(a);
+    b = parseInt(b);
+    return a / b;
+  });
+  var $div = $('<div>');
+  $div.text(divide);
+  $('#output').append($div);
+}
+
+function subNums() {
+  var sub = numbers.reduce(function(a, b) {
+    a = parseInt(a);
+    b = parseInt(b);
+    return a - b;
+  });
+  var $div = $('<div>');
+  $div.text(sub);
   $('#output').append($div);
 }
